bfbb0d62175a998b2977b5f8135e2ee798a3d195
405742-VegaAngel-1w2 <nahuelvegacsharp@gmail.com>
2025-01-19 22:27:25 -0300

    .

---
+++ UnitConverterBackEnd/src/main/java/org/example/unitconverterbackend/UnitConverterBackEndApplication.java
@@ -0,0 +1,13 @@
+package org.example.unitconverterbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnitConverterBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UnitConverterBackEndApplication.class, args);
+    }
+
+}
---
+++ UnitConverterBackEnd/src/test/java/org/example/unitconverterbackend/UnitConverterBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.unitconverterbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnitConverterBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
